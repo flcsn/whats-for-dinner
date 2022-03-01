aef3909468c0286c1391de42b8d4f4b255e5deef
flcsn <flacson98@gmail.com>
2022-03-01 20:53:26 +0800

    added search results html and sample items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { GiChickenOven } from 'react-icons/gi'
 
 const App = () => {
   return (
@@ -35,8 +36,28 @@ const App = () => {
         <div className='status-header'>
           <p className='status-line'>Searching for INGREDIENT recipes that is TAG...</p>
         </div>
-        <div>
-
+        <div className='search-results'>
+          <div className='search-item'>
+            <GiChickenOven style={{ height: '100px', width: '100px' }}/>
+            <div className='search-item-details'>
+              <h2 className='search-item-name'>Roast Chicken</h2>
+              <a className='search-item-link'>Link to Recipe</a>
+            </div>
+          </div>
+          <div className='search-item'>
+            <GiChickenOven style={{ height: '100px', width: '100px' }}/>
+            <div className='search-item-details'>
+              <h2 className='search-item-name'>Roast Chicken</h2>
+              <a className='search-item-link'>Link to Recipe</a>
+            </div>
+          </div>
+          <div className='search-item'>
+            <GiChickenOven style={{ height: '100px', width: '100px' }}/>
+            <div className='search-item-details'>
+              <h2 className='search-item-name'>Roast Chicken</h2>
+              <a className='search-item-link'>Link to Recipe</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
