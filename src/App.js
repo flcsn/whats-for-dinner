a60ed7c3700e22a4c64d2de1fead4d2c532a3828
flcsn <flacson98@gmail.com>
2022-03-01 18:12:32 +0800

    added basic html layout for left container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='left-main-container'>
+        <h2 className='app-title'>What&apos;s for Dinner?</h2>
+        <div>
+          <p className='step-label'>Step 1: Select an ingredient </p>
+        </div>
+        <div>
+          <p className='step-label'>Step 2: Select a tag</p>
+        </div>
+        <div>
+          <p className='step-label'>Step 3: Find dinner ideas!</p>
+        </div>
+      </div>
+      <div className='right-main-container'>
+        <p>Hello!</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
