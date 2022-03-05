44385d802a0178f955c58185ad7f8037266f01ca
flcsn <flacson98@gmail.com>
2022-03-05 21:12:24 +0800

    recipe service can now search for recipes that have an ingredient/tag value of "Any"

--- src/services/recipeService.js
+++ src/services/recipeService.js
@@ -3,17 +3,19 @@ import axios from 'axios'
 const tastyURL = 'https://tasty.p.rapidapi.com/recipes/list'
 
 const getRecipe = async (ingredient, tag) => {
+  const params = {
+    from: '0',
+    size: '20',
+    ...(tag !== 'any' && { tags: tag }),
+    ...(ingredient !== 'any' && { q: ingredient })
+  }
+
   const response = await axios.get(tastyURL, {
     headers: {
       'x-rapidapi-host': 'tasty.p.rapidapi.com',
       'x-rapidapi-key': '46c7b8d39fmsh0a692f6a9564c55p188574jsn0ac567d9bf39'
     },
-    params: {
-      from: '0',
-      size: '20',
-      tags: tag,
-      q: ingredient
-    }
+    params
   })
 
   return response.data
